1336cef61ad91567b3c5b9e2b83a969c34fcb9a0
Jeff Inman <jeffery.inman@hotmail.com>
2021-10-09 22:16:16 -0700

    adjusted struff

--- server.js
+++ server.js
@@ -6,18 +6,17 @@ require('dotenv').config();
 
 const cors = require('cors');
 
-// const { request } = require('express');
 
 const app = express();
 
 app.use(cors());
 
-
 const PORT = process.env.PORT || 3001;
 
-
-
-
+app.get('/', (request, response) => {
+  response.send('greetings from city explorer server');
+});
+app.get('/weather', (request, response) => response.json({name: 'captain'}));
 
 
 
