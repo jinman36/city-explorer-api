32a96ac274023636c8f5d6f695c7035ad2f68806
Jeff Inman <jeffery.inman@hotmail.com>
2021-10-11 20:43:47 -0700

    worked through backend development in class

--- server.js
+++ server.js
@@ -10,9 +10,37 @@ const PORT = process.env.PORT || 3001;
 
 
 app.get('/', (request, response) => response.status(200).send('this is the root.'));
+
+app.get('/weather', handleWeather);
+
 app.get('*', (request, response) => {
   response.status(404).send('Page not found');
 });
+
+function handleWeather(request, response) {
+  console.log('query parameters:', request.query);
+  let { lat, lon, searchQuery } = request.query;
+  // console.log(lat, lon, searchQuery);
+
+  let foundCity = weather.find(object => object.city_name.toLowerCase() === searchQuery.toLowerCase());
+  console.log(foundCity.data);
+
+  try {
+    const weatherArray = foundCity.data.map(day => {
+      return day.weather.description;
+    });
+    console.log(weatherArray);
+  }
+  catch (error) {
+    console.log('Cannot find City');
+    response.status(404).send('Unable to find CIty');
+  }
+
+
+
+  response.status(200).send('weather route works');
+}
+
 
 
 app.listen(PORT, () => console.log('Listening on Port: ', PORT));
