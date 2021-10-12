d6b475f9ec905e236d5c973075ee090c0352732e
Jeff Inman <jeffery.inman@hotmail.com>
2021-10-11 19:11:23 -0700

    sever running with no errors

--- server.js
+++ server.js
@@ -1,24 +1,18 @@
 'use strict';
 
 const express = require('express');
-
 require('dotenv').config();
-
 const cors = require('cors');
-
-
+const weather = require('./data/weather.json');
 const app = express();
-
 app.use(cors());
-
 const PORT = process.env.PORT || 3001;
 
-app.get('/', (request, response) => {
-  response.send('greetings from city explorer server');
+
+app.get('/', (request, response) => response.status(200).send('this is the root.'));
+app.get('*', (request, response) => {
+  response.status(404).send('Page not found');
 });
-app.get('/weather', (request, response) => response.json({name: 'captain'}));
 
 
-
-
-app.listen(PORT, () => console.log('listening on Port:', PORT));
+app.listen(PORT, () => console.log('Listening on Port: ', PORT));
