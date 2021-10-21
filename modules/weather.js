0a711fb16857e81a5cf5ba3b6fab156c6a56565c
Jeff Inman <jeffery.inman@hotmail.com>
2021-10-20 20:43:35 -0700

    cache assignment card complete

--- modules/weather.js
+++ modules/weather.js
@@ -1,21 +1,35 @@
 'use strict';
-
+let cache = require('./Cache');
 const axios = require('axios');
 
 async function weatherData(request, response) {
   let { lat, lon } = request.query;
   let weatherUrl = `https://api.weatherbit.io/v2.0/forecast/daily?&lat=${lat}&lon=${lon}&units=I&key=${process.env.REACT_APP_WEATHER_API_KEY}`;
-  console.log(weatherUrl);
-  try {
-    let weatherData = await axios.get(weatherUrl);
-    let weatherObject = weatherData.data;
-    const weatherArray = weatherObject.data.map(day => new Forecast(day));
-    response.status(200).send(weatherArray);
+  // console.log(weatherUrl);
+
+  if (cache[lat, lon] &&
+    Date.now() - cache[lat, lon].timestamp < 1000 * 10) {
+
+    response.status(200).send(cache[lat, lon]);
+    console.log(cache, 'cache hit');
   }
-  catch (error) {
-    response.status(500).send('Unable to get Forecast');
+
+  else {
+    try {
+      let weatherData = await axios.get(weatherUrl);
+      let weatherObject = weatherData.data;
+      const weatherArray = weatherObject.data.map(day => new Forecast(day));
+      cache[lat, lon] = {
+        latLon: weatherArray,
+        timestamp: Date.now()
+      };
+      console.log('Weather Cache empty');
+      response.status(200).send(weatherArray);
+    }
+    catch (error) {
+      response.status(500).send('Unable to get Forecast');
+    }
   }
-  response.status(200).send('weather route works');
 }
 
 class Forecast {
